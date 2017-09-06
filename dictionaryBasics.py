30a71cbd2736e554c6df8c9be480debc767c0d1b
Anastasia Lavrikova <anastasialavrikova@Anastasias-MacBook-Air.local>
2017-09-06 15:07:44 -0700

    initial commit, task completed

---
+++ dictionaryBasics.py
@@ -0,0 +1,8 @@
+myInfo = {"name": "Juliana", "age": "26", "country": "Costa Rica", "language": "French"}
+def printData():
+	print "My name is: " + myInfo["name"]
+	print "My age is: " + myInfo["age"]
+	print "My country is: " + myInfo["country"]
+	print "My language is: " + myInfo["language"]
+
+printData()	
